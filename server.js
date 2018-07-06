92867a55d2d8b60bd7e1067c260c7c1cbb4e88b3
Consuella Monk <cmonk@Consuellas-MBP.fios-router.home>
2018-07-06 18:49:45 -0400

    fixing file structure

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 3000;
 ///set for mongoose dependency ////
 const mongoose = require("mongoose");
 ///sert for heroku for hosting /////
-const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/parenthelp';
 
 
 ////connect to mongo ////
@@ -20,21 +20,58 @@ mongoose.connection.on("open", ()=>{
 
 
 ///Middleware/////
-
-
+app.use(express.urlencoded({extended:true}));
+app.use(express.json()) //
 
 
 
 ////Routes ////
+// ___________________
+// 7 Restful Routes
+// ___________________
+// Index  : GET    '/parents'          1/7
+// Show   : GET    '/parents/:id'      2/7
+// New    : GET    '/parents/new'      3/7
+// Create : POST   '/parents'          4/7
+// Edit   : GET    '/parents/:id/edit' 5/7
+// Update : PUT    '/parents/:id'      6/7
+// Delete : DELETE '/parents/:id'      7/7
+
+
 ///create a basic new route ////
+
+//Index : GET '/parents'  1/7
 app.get("/", (req, res)=>{
   res.send("this works");
 });
 
+
+///New : GET //  '/products/new' 3/7
 app.get("/new", (req, res)=>{
   res.render("new.ejs");
 });
 
+//show : GET '/parents/:id' 2/7
+
+
+
+
+// create: POST '/products' 4/7
+app.post('/parents/', (req, res)=>{
+    res.send('received');
+});
+
+//edit: GET '/parents/:id/edit' 5/7
+
+
+//update: PUT '/parents/:id'  6/7
+
+
+
+//delete: DELETE '/parents/:id' 7/7
+
+
+///Parent Log in??
 
 
 
