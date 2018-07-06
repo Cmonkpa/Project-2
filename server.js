5ec82e5d0faca257373646bf86c754b05aab82ec
Consuella Monk <cmonk@Consuellas-MBP.fios-router.home>
2018-07-06 14:06:16 -0400

    building new parent form

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
+///Dependencies/////
 const express = require("express");
 const app = express();
+////listens on active port (3000 is local)////
 const PORT = process.env.PORT || 3000;
+///set for mongoose dependency ////
+const mongoose = require("mongoose");
+///sert for heroku for hosting /////
+const mongoUri =  process.env.MONGODB_URI || 'mongodb://localhost:27017/grocery_app_dev';
+ mongoose.connect(mongoUri);
 
-///Routes
+
+
+
+////connect to mongo ////
+mongoose.connect(mongoUri, {useNewURLParser: true});
+mongoose.connection.on("open", ()=>{
+  console.log("connected to mongoose!!");
+})
+/// Controllers///
+
+
+
+
+///Middleware/////
+
+
+
+
+
+////Routes ////
+///create a basic new route ////
 app.get("/", (req, res)=>{
   res.send("this works");
 });
 
-//listener
+
+
+
+//////listener//////
 app.listen(PORT, ()=>{
   console.log("listening...");
-})
+});
