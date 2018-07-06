f205e16fefce1afd4ac17812f948457619d09bc7
Consuella Monk <cmonk@Consuellas-MBP.fios-router.home>
2018-07-06 11:40:07 -0400

    updating name

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
 
+///Routes
 app.get("/", (req, res)=>{
   res.send("this works");
 });
+
+//listener
+app.listen(PORT, ()=>{
+  console.log("listening...");
+})
