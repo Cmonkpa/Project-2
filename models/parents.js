636349b05780608251a6887a2bd71d85d166960c
Consuella Monk <cmonk@Consuellas-MBP.fios-router.home>
2018-07-09 11:20:38 -0400

    fixing view pages

--- models/parents.js
+++ models/parents.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const parentSchema = new mongoose.Schema({
   name: {type: String, required: true },
   username: {type: String, required: true },
-  numOfKids: {type: Number, min: 1, max: 6+ },
+  numOfKids: {type: Number, min: 1, max: 6 },
   rangeOfKids: {type: String},
   Over21: Boolean
 
--- server.js
+++ server.js
@@ -45,22 +45,25 @@ app.use(methodOverride('_method'))
 ///create a basic new route ////
 
 //Index : GET '/parents'  1/7
-app.get("/parents", (req, res)=>{
-  res.send("index.ejs");
+app.get("/Parent-Helper", (req, res)=>{
+  res.render("index.ejs");
 });
 
 
 ///New : GET //  '/products/new' 3/7
-app.get("/new", (req, res)=>{
+app.get("/Parent-Helper/new", (req, res)=>{
   res.render("new.ejs");
 });
 
 //show : GET '/parents/:id' 2/7
-app.get('/parents/:id',(req, res)=>{
-  Parent.findById(req.params.id, (err, parent)=>{
-    res.send(parent);
-  });
+app.get("/Parent-Helper/show", (req, res)=>{
+  res.render("show.ejs");
 });
+// app.get('/parents/:id',(req, res)=>{
+//   Parent.findById(req.params.id, (err, parent)=>{
+//     res.send(parent);
+//   });
+// });
 
 
 
@@ -70,28 +73,32 @@ app.get('/parents/:id',(req, res)=>{
 //     res.send('received');
 // });
 
-app.post('/parents/', (req, res)=>{
-  if(req.body.over21 === 'on'){
-    req.body.over21 = true;
-  } else {
-    req.body.over21 = false;
-  }
-  Parent.create(req.body, err, createdParent)=>{
-    res.send(createdParent);
-  });
-});
+// app.post('/parents/', (req, res)=>{
+//   if(req.body.over21 === 'on'){
+//     req.body.over21 = true;
+//   } else {
+//     req.body.over21 = false;
+//   }
+//   Parent.create(req.body, err, createdParent)=>{
+//     res.send(createdParent);
+//   });
+// });
 
 //edit: GET '/parents/:id/edit' 5/7
-app.get("/parents/:id/edit", (req, res)=>{
-  Parent.findById(req.params.id, (err, foundParent)={
-    res.render(
-          "edit.ejs",
-          {
-              parent: foundParent
-          }
-    );
-  });
+app.get("/edit", (req, res)=>{
+  res.render("edit.ejs");
 });
+
+// app.get("/parents/:id/edit", (req, res)=>{
+//   Parent.findById(req.params.id, (err, foundParent)={
+//     res.render(
+//           "edit.ejs",
+//           {
+//               parent: foundParent
+//           }
+//     );
+//   });
+// });
 
 //update: PUT '/parents/:id'  6/7
 //need to update this code...this is placeholder for update route.
